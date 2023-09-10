12d0ed0a3300c7744304484d6ff0453d65d93eb9
zjj20212035 <144603940+zjj20212035@users.noreply.github.com>
2023-09-10 21:32:54 +0800

    Add files via upload

---
+++ AW-SPCA-PSD/AW_SPCA_PSD.m
@@ -0,0 +1,51 @@
+function [ id,LP ] = AW_SPCA_PSD( X,lambda )
+% Adaptive Weighted Sparse Principal Component Analysis with PSD constraint
+% X: data matrix, each column is a sample
+% lambda: the regularization parameter for l2,1-norm
+[m,n] = size(X);
+X = X - repmat(mean(X,2),1,n);
+v = zeros( m , 1 );
+one = ones( n , 1);
+W1 = eye(n);
+W2 = eye(m);
+E1 = X * W1 * X';
+I = eye(m);
+
+delta = inf;
+k=1;
+LP = zeros(1,50);
+DELTA = zeros(1,100);
+Niter = 100;
+
+while delta > 10^-5
+    A = ( E1 - v * one' * W1 * X') / ( E1 + lambda * W2 + 0.001*I);
+    A = Keep_PSD(A);
+    v = ( X * W1 - A * X * W1 ) * one / ( one' * W1 * one );
+
+    W1 = diag( 1./( 2 * sqrt( sum(( X - A*X - v*one' ).^2 ) + 0.001) ) );
+    W2 = diag( 1./( 2 * sqrt( sum( A .^2 ) + 0.001) ) );
+    E1 = X*W1*X';
+    Lp = norm( sqrt( sum(( X - A*X - v*one' ).^2 ) ), 1) + lambda * norm( sqrt( sum( A.^2 ) ), 1);
+    if k == 1
+        LP(1) = Lp;
+    else
+        LP(k) = Lp;
+        delta = abs(LP(k) - LP(k-1));
+        DELTA(k-1) = delta;
+    end
+    
+    if k > Niter
+        break
+    end
+    
+    k = k+1;
+end
+ 
+sqA = (A.^2);
+sumA = sum(sqA);
+[~,id] = sort(sumA,'descend');
+
+
+end
+
+
---
+++ CSPCA-PSD/CSPCA_PSD.m
@@ -0,0 +1,47 @@
+function [id,LP] = CSPCA_PSD(X,alpha,beta)
+% Convex Sparse Principal Component Analysis with PSD constraint
+% X: data matrix, each column is a sample
+% alpha: the regularization parameter for l2,1-norm
+% beta: the regularization parameter for nuclear norm
+[d,n] = size(X);
+W = diag(rand(1,d));
+X = X - repmat(mean(X,2),1,n);
+E = (W.'*X-X)';
+D1 = diag(1./(2*sqrt(sum(E.^2,2)+0.001)));
+D2 = diag(1./(2*sqrt(sum(W.^2,2)+0.001)));
+
+LP = zeros(1,100);
+delta = 1;
+t = 1;
+I = eye(d);
+Niter = 100;
+
+while delta > 10^-5
+    if t==1
+        nuclear_norm = sum(diag(W));  
+        Lp = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta * nuclear_norm;
+        LP(1) = Lp;
+    end
+    W = (X*D1*X' + alpha*D2 + 0.001*I)\(X*D1*X'-beta*I);
+    W = Keep_PSD(W);
+    E = (W.'*X-X)';
+    D1 = diag(1./(2*sqrt(sum(E.^2,2)+0.001)));
+    D2 = diag(1./(2*sqrt(sum(W.^2,2)+0.001)));
+    
+    nuclear_norm = sum(diag(W));
+    Lp1 = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta *  nuclear_norm;
+    delta = abs(Lp1-Lp);
+    LP(t+1) = Lp1;
+    Lp = Lp1;
+    t = t+1;
+    if t > Niter
+        break
+    end
+
+end
+
+sqW = (W.^2);
+sumW = sum(sqW);
+[~,id] = sort(sumW,'descend');
+end
+
