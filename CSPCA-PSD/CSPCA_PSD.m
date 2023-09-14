0eb2e283e7dbc48f9f6fbf63d949a19373b63b0c
zjj20212035 <144603940+zjj20212035@users.noreply.github.com>
2023-09-14 09:42:45 +0800

    CSPCA_PSD.m

--- CSPCA-PSD/CSPCA_PSD.m
+++ CSPCA-PSD/CSPCA_PSD.m
@@ -1,8 +1,11 @@
-function [id,LP] = CSPCA_PSD(X,alpha,beta)
+function [id,OBJ] = CSPCA_PSD(X,alpha,beta)
 % Convex Sparse Principal Component Analysis with PSD constraint
 % X: data matrix, each column is a sample
 % alpha: the regularization parameter for l2,1-norm
 % beta: the regularization parameter for nuclear norm
+% id: The rank of features
+% OBJ: The objective function value
+
 [d,n] = size(X);
 W = diag(rand(1,d));
 X = X - repmat(mean(X,2),1,n);
@@ -10,7 +13,7 @@ E = (W.'*X-X)';
 D1 = diag(1./(2*sqrt(sum(E.^2,2)+0.001)));
 D2 = diag(1./(2*sqrt(sum(W.^2,2)+0.001)));
 
-LP = zeros(1,100);
+OBJ = zeros(1,100);
 delta = 1;
 t = 1;
 I = eye(d);
@@ -19,8 +22,8 @@ Niter = 100;
 while delta > 10^-5
     if t==1
         nuclear_norm = sum(diag(W));  
-        Lp = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta * nuclear_norm;
-        LP(1) = Lp;
+        obj = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta * nuclear_norm;
+        OBJ(1) = obj;
     end
     W = (X*D1*X' + alpha*D2 + 0.001*I)\(X*D1*X'-beta*I);
     W = Keep_PSD(W);
@@ -29,10 +32,10 @@ while delta > 10^-5
     D2 = diag(1./(2*sqrt(sum(W.^2,2)+0.001)));
     
     nuclear_norm = sum(diag(W));
-    Lp1 = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta *  nuclear_norm;
-    delta = abs(Lp1-Lp);
-    LP(t+1) = Lp1;
-    Lp = Lp1;
+    obj1 = norm(sqrt(sum(E.^2,2)),1) + alpha * norm(sqrt(sum(W.^2,2)),1) + beta *  nuclear_norm;
+    delta = abs(obj1-obj);
+    OBJ(t+1) = obj1;
+    obj = obj1;
     t = t+1;
     if t > Niter
         break
